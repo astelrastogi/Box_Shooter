5c9109225e747d1294b40918d0a28f51e48c0a8b
astelrastogi <aasthar3@gmail.com>
2023-07-14 01:58:17 +0530

    Initial Commit

---
+++ Assets/NextLevelLoad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision newCollision)
+	{
+		// only do stuff if hit by a projectile
+		if (newCollision.gameObject.tag == "Projectile") {
+			// call the RestartGame function in the game manager
+			GameManager.gm.NextLevel();
+		}
+	}
+}
